a388c8338392f9ce092fd712d4fdb3891bce9f0b
mikerockin <mrmikhail88@gmail.com>
2020-10-22 19:21:37 +0500

    ''

--- bot.py
+++ bot.py
@@ -1,23 +1,37 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 from telegram.ext import MessageHandler
-from telegram.ext import Filters
+from settings import TG_TOKEN, TG_API_URL
+import requests
+from bs4 import BeautifulSoup
+
 
 
 def sms(bot, update):
     print('Кто-то отправил команду /start. Что мне делать?')
-    bot.message.reply_text('Здравствуйте, {}! \n'
-    'Поговорите со мной!'.format(bot.message.chat.first_name))
+    my_keyboard = ReplyKeyboardMarkup([['Анекдот', 'начать']], resize_keyboard=True)
+    bot.message.reply_text('Приветствую тебя о , {}! \n'
+    'Расскажешь мне о залупе лягушки?'.format(bot.message.chat.first_name), reply_markup=my_keyboard)
     print(bot.message)
+
+def get_anecdote(bot, update):
+    receive = requests.get('http://anekdotme.ru/random')
+    page = BeautifulSoup(receive.text, "html.parser")
+    find = page.select('.anekdot_text')
+    for text in find:
+        page =(text.getText().strip())
+    bot.message.reply_text(page)
 
 def parrot(bot, update):
     print(bot.message.text)
     bot.message.reply_text(bot.message.text)
 
 def main():
-    my_bot = Updater("1352673032:AAEfDbWv6Up8U1Z2cGb9CbyCc7j9BG7zIpE","https://telegg.ru/orig/bot", use_context=True)
+    my_bot = Updater(TG_TOKEN, TG_API_URL, use_context=True)
+    my_bot.dispatcher.add_handler(CommandHandler('start', sms))
 
-    my_bot.dispatcher.add_handler(CommandHandler('start',sms))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Начать'), sms))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Анекдот'), get_anecdote))
     my_bot.dispatcher.add_handler(MessageHandler(Filters.text, parrot))
     my_bot.start_polling()
     my_bot.idle()
